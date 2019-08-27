327598c900aa4e400a4f9a9f43b092833f9a8cd0
akhilpgvr <49089198+akhilpgvr@users.noreply.github.com>
2019-08-27 21:01:22 +0530

    Update delete_elements_of_array.c

--- delete_elements_of_array.c
+++ delete_elements_of_array.c
@@ -1,4 +1,4 @@
-#This code is to deleting element in an array 
+//This code is to deleting element in an array 
 
 
 #include<stdio.h>
@@ -6,21 +6,21 @@ int main()
 {
  int a[10],i,pos,num;
  printf("Enter the number of elements\t");
- scanf("%d",&num);                                      #Inputng number of elements 
+ scanf("%d",&num);                                      //Inputng number of elements 
  printf("Enter the elements in to array");
  for(i=0;i<num;i++)
  {
-  scanf("%d",&a[i]);              #Inputing elements of array
+  scanf("%d",&a[i]);              //Inputing elements of array
  }
  printf("Enter the position for deleting\t");
- scanf("%d",&pos);                          #Inputing the position of element for deleting
+ scanf("%d",&pos);                          //Inputing the position of element for deleting
  for(i=pos-1;i<=num;i++)
  {
-  a[i] = a[i+1];                   #Replacing i+1'th element by i'th element
+  a[i] = a[i+1];                   //Replacing i+1'th element by i'th element
  }
  printf("The required array is");
  for(i=0;i<num-1;i++)
  {
-  printf(" %d",a[i]);                   #displaying array
+  printf(" %d",a[i]);                   //displaying array
  }
 }
